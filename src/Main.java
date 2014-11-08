121ea94557ea14e869afb735d8da37ab236dffca
ysm111 <elegant-belltree@gmail.com>
2014-11-08 15:25:56 +0900

    TreeSetからLinkedListに変更

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * 8進数と16進数で共に循環数列数になる数を表示するプログラム
@@ -9,40 +7,29 @@ import java.util.TreeSet;
 public class Main {
     public static void main(String[] args){
         /**
-         * 循環数列数を格納するTreeSet
+         * 循環数列数を格納するList
+         * (要素の追加が多く、添字指定が少ないのでLinkedListを使っている)
          */
-        Set<Integer> seq=new TreeSet<Integer>();
-        /**
-         * TreeSetのイテレータ
-         */
-        Iterator<Integer> it;
+        List<Integer> seq=new LinkedList<Integer>();
         /**
          * 循環数列数(16進数)
          */
         StringBuilder hex=new StringBuilder();
 
         try{
-            //12桁表記で循環数列数の元となる数jが8進数,16進数ともに0で始まらない範囲で繰り返す
+            //循環数列数の元となる数jが8進数,16進数ともに0で始まらない範囲で繰り返す
             for(int j=(int)Math.pow(2,9);j<(int)Math.pow(2,12);j++) {
                 //16進数のjを2回繰り返し、循環数列数を生成
                 for(int i=0;i<2;i++) {
                     hex.append(Integer.toHexString(j));
                 }
-                //生成された循環数列数をTreeSetに追加
+                //生成された循環数列数をlistに追加
                 seq.add(Integer.parseInt(hex.toString(), 16));
-                //hexを初期化
+                //StringBuilderを初期化
                 hex.setLength(0);
             }
-            //イテレータをセット
-            it=seq.iterator();
-            //イテレータを10回動かす
-            for(int i=0;i<10 && it.hasNext();i++){
-                it.next();
-            }
-            //TreeSetの11個目に要素があるならばそれを表示
-            if(it.hasNext()) {
-                System.out.println(it.next());
-            }
+            //11番目の要素を表示
+            System.out.println(seq.get(10));
         }catch(Exception e){
             //例外の内容を表示
             e.printStackTrace();
